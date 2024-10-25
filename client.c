059fd831d4b33147fc989b4c9b3a3e343ce6985d
peu77 <emilebert@icloud.com>
2024-10-25 14:13:01 +0200

    add sending messages

--- client.c
+++ client.c
@@ -6,30 +6,46 @@
 #include "ft_printf.h"
 
 
-void send_char_to_pid(int pid, char c)
+void send_char_to_pid(const int pid,const char c)
 {
     for(int i = 0; i < 8; i++)
     {
-        unsigned long long bit = (c >> i) & 1;
+        const int bit = (c >> i) & 1;
+        kill(pid, 30 + bit);
+        usleep(500);
+    }
+}
+
+void send_pid_to_pid(const pid_t pid)
+{
+    const pid_t me = getpid();
+
+    for(int i = 0; i < 32; i++)
+    {
+        const int bit = (me >> i) & 1;
         kill(pid, 30 + bit);
         usleep(500);
     }
 }
 
 
-int main(int argc, char** argv)
+int main(const int argc, char** argv)
 {
     (void) argc;
-    int pid = ft_atoi(argv[1]);
+    const int pid = ft_atoi(argv[1]);
+    const pid_t my_pid = getpid();
 
-    ft_printf("pid send to %d\n", pid);
+    ft_printf("pid send to %d my_pid: %ul\n", pid, my_pid);
 
-    char* str = argv[2];
+    const char* str = argv[2];
+
+    send_pid_to_pid(pid);
 
     while(*str)
     {
         send_char_to_pid(pid, *str);
         str++;
     }
+    send_char_to_pid(pid, 0);
 
 }
--- server.c
+++ server.c
@@ -3,14 +3,37 @@
 #include <sys/_types/_pid_t.h>
 #include <unistd.h>
 
-void signal_handler(int sig){
+void signal_handler(pid_t pid){
     static int byte_i = 0;
     static char c;
+    static pid_t client_pid;
+    static int char_i = 0;
 
-    c |= (sig - 30) << byte_i;
+    const int bit = pid - 30;
+
+    if(char_i == 0)
+    {
+        client_pid |= ((pid_t)bit) << byte_i;
+        byte_i++;
+        if(byte_i == 32)
+        {
+            printf("client_id: %ul\n", client_pid);
+            char_i++;
+            byte_i = 0;
+        }
+        return;
+    }
+
+    c |= (pid - 30) << (byte_i);
     byte_i += 1;
     if(byte_i == 8){
         write(1, &c, 1);
+        if(c == 0)
+        {
+            write(1, "\n", 1);
+            client_pid = 0;
+            char_i = 0;
+        }
         c = 0;
         byte_i = 0;
     }
