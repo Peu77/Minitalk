c3d2d7687a79e05fe1c78c7d9a1b72971a2b2ad7
peu77 <emilebert@icloud.com>
2024-10-25 15:11:53 +0200

    change utils

--- utils.c
+++ utils.c
@@ -1,13 +1,18 @@
 #include <sys/signal.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdio.h>
 
 void send_n_bytes_to_pid(pid_t pid, unsigned long data, int n)
 {
-    for(int i = 0; i < n * 8; i++)
+    int i = 0;
+
+    while (i < n * 8)
     {
         const int bit = (data >> i) & 1;
         kill(pid, 30 + bit);
         usleep(100);
+
+        i++;
     }
 }
