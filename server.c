491fe4007141bcd3f7ef71cf1b984d5c9377c852
peu77 <emilebert@icloud.com>
2024-10-30 17:04:20 +0100

    norm correct server and server_utils

--- server.c
+++ server.c
@@ -1,91 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eebert <eebert@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 16:51:46 by eebert            #+#    #+#             */
+/*   Updated: 2024/10/30 17:03:51 by eebert           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include "server.h"
 #include "utils.h"
 #include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <sys/_types/_pid_t.h>
 #include <sys/signal.h>
 #include <unistd.h>
-#include "ft_printf.h"
-#include <stdio.h>
-#include <stdbool.h>
-#include "server.h"
 
-static bool handle_receive_pid(t_client_info* client_info)
+static bool	handle_receive_pid(t_client_info *client_info)
 {
-    printf("\nreceived pid: %d\n", client_info->pid);
-    return true;
+	printf("\nreceived pid: %d\n", client_info->pid);
+	return (true);
 }
 
-static bool handle_receive_size(t_client_info* client_info)
+static bool	handle_receive_size(t_client_info *client_info)
 {
-    printf("\nreceived size: %zu\n", client_info->size);
-    client_info->str = malloc(client_info->size + 1);
-    client_info->char_i = 0;
-    if (!client_info->str)
-    {
-        ft_printf("failed to allocate memory\n");
-        exit(1);
-    }
-    return true;
+	printf("\nreceived size: %zu\n", client_info->size);
+	client_info->str = malloc(client_info->size + 1);
+	client_info->char_i = 0;
+	if (!client_info->str)
+	{
+		ft_printf("failed to allocate memory\n");
+		exit(1);
+	}
+	return (true);
 }
 
-static bool handle_receive_data(t_client_info* client_info)
+static bool	handle_receive_data(t_client_info *client_info)
 {
-    if (client_info->char_i++ < client_info->size)
-    {
-        print_ascii_loading_animation(client_info->char_i, client_info->size);
-        client_info->str[client_info->char_i] = client_info->current_char;
-    }
-    else
-    {
-        write(1, "\n", 1);
-        write(1, client_info->str, client_info->char_i);
-        write(1, "\n", 1);
-        printf("sending response\n");
-        send_n_bytes_to_pid(client_info->pid, client_info->char_i, sizeof(size_t), true);
-        reset_client_info(client_info);
-        return true;
-    }
-    client_info->current_char = 0;
-    return false;
+	if (client_info->char_i++ < client_info->size)
+	{
+		print_ascii_loading_animation(client_info->char_i, client_info->size);
+		client_info->str[client_info->char_i] = client_info->current_char;
+	}
+	else
+	{
+		write(1, "\n", 1);
+		write(1, client_info->str, client_info->char_i);
+		write(1, "\n", 1);
+		printf("sending response\n");
+		send_n_bytes_to_pid(client_info->pid, client_info->char_i,
+			sizeof(size_t), true);
+		reset_client_info(client_info);
+		return (true);
+	}
+	client_info->current_char = 0;
+	return (false);
 }
 
-void signal_handler(int signal)
+void	signal_handler(int signal)
 {
-    static t_client_info client_info;
-    const int bit = signal - 30;
-    int i;
-    const static t_receive_step_mapper receive_step_mapper[] = {
-        {RECEIVE_PID, handle_receive_pid, sizeof(pid_t), &client_info.pid},
-        {RECEIVE_SIZE, handle_receive_size, sizeof(size_t), &client_info.size},
-        {RECEIVE_DATA, handle_receive_data, sizeof(char), &client_info.current_char}
-    };
+	static t_client_info				client_info;
+	const int							bit = signal - 30;
+	const static t_receive_step_mapper	receive_step_mapper[] = {{RECEIVE_PID,
+		handle_receive_pid, sizeof(pid_t), &client_info.pid}, {RECEIVE_SIZE,
+		handle_receive_size, sizeof(size_t), &client_info.size},
+	{RECEIVE_DATA, handle_receive_data, sizeof(char),
+		&client_info.current_char}};
 
-    i = 0;
-    if(signal == SIGINT)
-    {
-        if (client_info.str)
-            free(client_info.str);
-        exit(0);
-    }
-
-    while (i != sizeof(receive_step_mapper) / sizeof(t_receive_step_mapper))
-    {
-        if (receive_step_mapper[i].step == client_info.step && read_n_bytes(
-            receive_step_mapper[i].data, receive_step_mapper[i].data_size, bit, client_info.step != RECEIVE_DATA))
-        {
-            if (receive_step_mapper[i].fun(&client_info))
-                client_info.step = (client_info.step + 1) % 3;
-            break;
-        }
-        i++;
-    }
+	const receive_step_mapper = get_receive_step_mapper(receive_step_mapper,
+			sizeof(receive_step_mapper)
+			/ sizeof(t_receive_step_mapper),
+			client_info.step);
+	if (signal == SIGINT)
+	{
+		if (client_info.str)
+			free(client_info.str);
+		exit(0);
+	}
+	if (read_n_bytes(receive_step_mapper[i].data,
+			receive_step_mapper[i].data_size, bit,
+			client_info.step != RECEIVE_DATA)
+		&& receive_step_mapper[i].fun(&client_info))
+		client_info.step = (client_info.step + 1) % 3;
 }
 
-int main(void)
+int	main(void)
 {
-    signal(SIGUSR1, signal_handler);
-    signal(SIGUSR2, signal_handler);
-    signal(SIGINT, signal_handler);
-    printf("pid: %d\n", getpid());
-    while (pause());
-    return (0);
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	signal(SIGINT, signal_handler);
+	printf("pid: %d\n", getpid());
+	while (pause())
+		;
+	return (0);
 }
--- server_utils.c
+++ server_utils.c
@@ -1,13 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eebert <eebert@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 16:57:47 by eebert            #+#    #+#             */
+/*   Updated: 2024/10/30 16:58:02 by eebert           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "server.h"
 #include <stdlib.h>
 
-void reset_client_info(t_client_info* client_info)
+void	reset_client_info(t_client_info *client_info)
 {
-    client_info->pid = 0;
-    client_info->size = 0;
-    if (client_info->str)
-    {
-        free(client_info->str);
-        client_info->str = NULL;
-    }
+	client_info->pid = 0;
+	client_info->size = 0;
+	if (client_info->str)
+	{
+		free(client_info->str);
+		client_info->str = NULL;
+	}
+}
+
+t_receive_step_mapper	*get_receive_step_mapper(t_receive_step_mapper *list,
+		size_t size, t_receive_step step)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (list[i].step == step)
+			return (&list[i]);
+		i++;
+	}
+	return (NULL);
 }
