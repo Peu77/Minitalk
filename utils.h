77d95d33862fa362cb128e21d1388f3991ea893e
peu77 <emilebert@icloud.com>
2024-10-30 16:46:49 +0100

    clean code and add animation

--- utils.h
+++ utils.h
@@ -2,14 +2,14 @@
 #define UTILS_H
 
 #include <sys/types.h>
+#include <stdbool.h>
 
-typedef enum e_receive_step
-{
-    RECEIVE_PID = 0,
-    RECEIVE_SIZE = 1,
-    RECEIVE_DATA = 2
-} t_receive_step;
+#define SEND_DELAY 60000
 
-void send_n_bytes_to_pid(pid_t pid, unsigned long data, int n);
+bool read_n_bytes(const void* nb, const size_t bytes, const int bit, const bool print_progress);
+
+void send_n_bytes_to_pid(pid_t pid, unsigned long data, int n, const bool print_progress);
+
+void print_ascii_loading_animation(const size_t current, const size_t total);
 
 #endif
