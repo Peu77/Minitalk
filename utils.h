bffff4e453fe5612a1f86ebe906a982c05a00bbb
Emil Ebert <emilebert@icloud.com>
2024-11-01 14:24:52 +0100

    utils.h norm correct

--- utils.h
+++ utils.h
@@ -1,15 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eebert <eebert@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 12:54:08 by eebert            #+#    #+#             */
+/*   Updated: 2024/11/01 14:23:44 by eebert           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef UTILS_H
-#define UTILS_H
+# define UTILS_H
 
-#include <sys/types.h>
-#include <stdbool.h>
+# include <stdbool.h>
+# include <sys/types.h>
 
-#define SEND_DELAY 60000
+# define SEND_DELAY 60000
 
-bool read_n_bytes(const void* nb, const size_t bytes, const int bit, const bool print_progress);
+bool	read_n_bytes(const void *nb, const size_t bytes, const int bit,
+			const bool print_progress);
 
-void send_n_bytes_to_pid(pid_t pid, unsigned long data, int n, const bool print_progress);
+void	send_n_bytes_to_pid(pid_t pid, unsigned long data, int n,
+			const bool print_progress);
 
-void print_ascii_loading_animation(const size_t current, const size_t total);
+void	print_ascii_loading_animation(const size_t current, const size_t total);
 
 #endif
