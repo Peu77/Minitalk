a6c5b51227342cac2daa83046a0f7c3e5ff37aee
peu77 <emilebert@icloud.com>
2024-11-02 11:44:19 +0100

    add bonus to phony

---
+++ build/CMakeFiles/Minitalk.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/eebert/Documents/projects/Minitalk/client.c" "CMakeFiles/Minitalk.dir/client.c.o" "gcc" "CMakeFiles/Minitalk.dir/client.c.o.d"
+  "/Users/eebert/Documents/projects/Minitalk/server.c" "CMakeFiles/Minitalk.dir/server.c.o" "gcc" "CMakeFiles/Minitalk.dir/server.c.o.d"
+  "/Users/eebert/Documents/projects/Minitalk/server_utils.c" "CMakeFiles/Minitalk.dir/server_utils.c.o" "gcc" "CMakeFiles/Minitalk.dir/server_utils.c.o.d"
+  "/Users/eebert/Documents/projects/Minitalk/utils.c" "CMakeFiles/Minitalk.dir/utils.c.o" "gcc" "CMakeFiles/Minitalk.dir/utils.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
